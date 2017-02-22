e53df6accf9719463988a475e03f2bad73abb526
Konstantin <games2010@mail.ru>
2017-02-22 23:07:15 +0300

    Переезд на Phalcon =)

--- public/index.php
+++ public/index.php
@@ -1,40 +1,64 @@
- <?php
+<?php
 
-use Zend\Mvc\Application;
-use Zend\Stdlib\ArrayUtils;
- 
-/**
- * This makes our life easier when dealing with paths. Everything is relative
- * to the application root now.
- */
-chdir(dirname(__DIR__));
+ini_set('error_reporting', E_ALL);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
 
-// Decline static file requests back to  the PHP built-in webserver
-if (php_sapi_name() === 'cli-server') {
-    $path = realpath(__DIR__ . parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
-    if (__FILE__ !== $path && is_file($path)) {
-        return false;
-    }
-    unset($path);
+use Phalcon\Loader;
+use Phalcon\Di\FactoryDefault;
+use Phalcon\Mvc\View;
+use Phalcon\Mvc\Url as UrlProvider;
+use Phalcon\Mvc\Application;
+use Phalcon\Mvc\Router;
+
+$loader = new Loader();
+
+$loader->registerDirs(
+        [
+             "../app/controllers/",
+             "../app/models/",
+        ]
+);
+
+$loader->register();
+
+
+
+
+// Create a DI
+$di = new FactoryDefault();
+// Setup the view component
+$di->set("view", function () {
+    $view = new View();
+    $view->setViewsDir("../app/views/");
+    return $view;
 }
+);
 
-// Composer autoloading
-include __DIR__ . '/../vendor/autoload.php';
 
-if (! class_exists(Application::class)) {
-    throw new RuntimeException(
-        "Unable to load application.\n"
-        . "- Type `composer install` if you are developing locally.\n"
-        . "- Type `vagrant ssh -c 'composer install'` if you are using Vagrant.\n"
-        . "- Type `docker-compose run zf composer install` if you are using Docker.\n"
-    );
+// Setup a base URI so that all generated URIs include the "tutorial" folder
+$di->set(
+        "url", function () {
+    $url = new UrlProvider();
+    $url->setBaseUri("/tutorial/");
+
+    return $url;
 }
+);
 
-// Retrieve configuration
-$appConfig = require __DIR__ . '/../config/application.config.php';
-if (file_exists(__DIR__ . '/../config/development.config.php')) {
-    $appConfig = ArrayUtils::merge($appConfig, require __DIR__ . '/../config/development.config.php');
-}
 
-// Run the application!
-Application::init($appConfig)->run();
+$router = new Router();
+$router->add(
+        "/gg/ww/ee/", [
+             "controller" => "zhopa",
+             "action" => "huj"
+        ]
+);
+
+$router->handle();
+
+$application = new Application($di);
+
+$response = $application->handle();
+
+$response->send();
